6d2e3eec4f5472f1774f379ef1bace58b360fd52
shanmugaraj022 <39941680+shanmugaraj022@users.noreply.github.com>
2018-06-05 13:18:39 +0530

    Create odd_or_even.py

---
+++ odd_or_even.py
@@ -0,0 +1,11 @@
+# vinobhavasri
+# your code goes here
+N=int(input("enter the values"))
+if(N<100000):
+    if(N%2==0):
+        print("even")
+    elif(N%3==0):
+        print("odd")
+else:
+    print("over flow")
+        
